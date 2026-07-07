8e43de6add9d108a04547bdd983df031e547f0f6
yuta17 <usgitan@gmail.com>
2026-07-08 07:46:12 +0900

    Show Hanon number only

--- scripts/validate-hanon.js
+++ scripts/validate-hanon.js
@@ -1,4 +1,6 @@
 const assert = require("node:assert/strict");
+const fs = require("node:fs");
+const path = require("node:path");
 const {
   HANON_NUMBERS,
   pickNumber,
@@ -13,5 +15,13 @@ assert.equal(pickNumber(() => 0.999), 5);
 for (let i = 0; i < 100; i += 1) {
   assert(HANON_NUMBERS.includes(pickNumber()));
 }
+
+const html = fs.readFileSync(path.join(__dirname, "../hanon/index.html"), "utf8");
+assert(html.includes("<title>ハノン</title>"));
+assert(html.includes("<h1>ハノン</h1>"));
+assert(html.includes('aria-label="ハノン"'));
+assert(html.includes("hanon.js?v=20260708-hanon-numberless"));
+assert(!html.includes("ハノン番号"));
+assert(!html.includes(">番号<"));
 
 console.log("Hanon validation passed");
